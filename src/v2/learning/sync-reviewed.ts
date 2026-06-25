f1111603f06a2fdc1167b516cc37db426da1cb69
dallascrilley <dallas@dallascrilley.com>
2026-06-25 09:31:31 -0500

    feat(promotion): queue cross-project promotion candidates

--- src/v2/learning/sync-reviewed.ts
+++ src/v2/learning/sync-reviewed.ts
@@ -4,6 +4,7 @@ import { replayBundles, saveSessionBundle } from "../instinct/bundle.js";
 import { buildSessionBundleFromLearnings } from "../instinct/from-learning.js";
 import { saveAllInstincts } from "../instinct/store.js";
 import { resolveProjectIdForSession } from "../project/resolve.js";
+import { refreshPromotionQueue } from "../promotion/queue.js";
 
 export async function syncReviewedLearningsToInstinctStore(input: {
   session: SourceSession;
@@ -26,6 +27,7 @@ export async function syncReviewedLearningsToInstinctStore(input: {
 
   const instincts = await replayBundles(projectId);
   await saveAllInstincts(projectId, instincts);
+  await refreshPromotionQueue(input.reviewedAt);
 
   return { bundleWritten: true, instinctCount: instincts.size, projectId };
 }
